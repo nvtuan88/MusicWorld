4d0319878a5d52433f0ff7fbb33f7254495ad8b0
Nguyễn Văn Tuấn <nvtuan88@gmail.com>
2020-11-19 18:43:48 +0700

    tuan

--- MusicWorld/MusicWorld/Startup.cs
+++ MusicWorld/MusicWorld/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using MusicWorld.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MusicWorld
 {
@@ -18,12 +21,17 @@ namespace MusicWorld
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<StoreDBContext>(opts => {
+                opts.UseSqlServer(
+                        Configuration["ConnectionStrings:MusicWorldConnection"]
+                    );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
